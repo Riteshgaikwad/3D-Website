a402c700569e0d6cf183c2b1568f635cddf766cb
Riteshgaikwad <gaikwadritesh2003@gmail.com>
2024-09-30 21:35:11 +0530

    highlights

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,6 +2,7 @@ import { useGSAP } from '@gsap/react'
 import React from 'react'
 import gsap from 'gsap'
 import { rightImg, watchImg } from '../utils'
+import VideoCarousel from './VideoCarousel'
 
 const Highlights = () => {
     useGSAP(()=>{
@@ -24,6 +25,7 @@ const Highlights = () => {
                     </p>
                 </div>
             </div>
+            <VideoCarousel/>
 
         </div>
 
